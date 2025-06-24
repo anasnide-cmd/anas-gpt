fadf91f847d38c9a87f9487b1c555c0ed4b78caf
anasnide <anasnidirproject@gmail.com>
2025-06-24 20:37:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-export const API_KEY = "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";
+export const firebaseConfig = {
+  apiKey: "AIzaSyDDBmrVIVFfDHIMa-VwcemtBEz54OJMtqc",
+  authDomain: "anas-gpt-backend.firebaseapp.com",
+  projectId: "anas-gpt-backend",
+  storageBucket: "anas-gpt-backend.appspot.com",
+  messagingSenderId: "3764495371",
+  appId: "1:3764495371:web:bf245b8309853c5fe6d30e",
+  measurementId: "G-DWLCNKV7QZ"
+};
