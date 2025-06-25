04d854997ff6bfadd767e94d467503a5a8aab6a0
anasnide <anasnidirproject@gmail.com>
2025-06-25 16:22:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+
 export const firebaseConfig = {
   apiKey: "AIzaSyDDBmrVIVFfDHIMa-VwcemtBEz54OJMtqc",
   authDomain: "anas-gpt-backend.firebaseapp.com",
@@ -8,3 +10,6 @@ export const firebaseConfig = {
   appId: "1:3764495371:web:bf245b8309853c5fe6d30e",
   measurementId: "G-DWLCNKV7QZ"
 };
+
+// ✅ Initialize app dan export
+export const app = initializeApp(firebaseConfig);
